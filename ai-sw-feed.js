13437b3db4fe584583e2dd0f239cd85ff93a7583
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2023-12-08 11:25:18 -0800

    initial commit

---
+++ ai-sw-feed.js
@@ -0,0 +1,71 @@
+// configure table columns
+  function createTableColumns(){
+
+    var tableColumns =   [
+        {'data': 'type', 'title': 'Document Type', "defaultContent": "", 'visible':false },
+        {'data': 'title', 'title': 'Title', 'className':'sw-title', "defaultContent": ""},
+        {'data': 'author', 'title': 'Author(s)', 'className':'sw-author', "defaultContent": ""},
+        {'data': 'year', 'title': 'Year', 'className':'sw-year', "defaultContent": ""},
+        {'data': 'url', 'title': 'URL', 'className':'sw-link', "defaultContent": "", "render": function(data, type, row, meta){
+              if(type === 'display'){
+                  data = '<a href="' + data + '">' + 'More information' + '</a>';
+            }
+            return data;
+         }},
+
+       ];
+  return tableColumns;
+}
+
+$(document).ready(function(){
+
+  $('#scholarWorksTable').html('<table cellpadding="0" cellspacing="0" border="0" class="display table" id="data-table-container" style="width:100%"></table>');
+
+  //this function creates the datatable and selects configuration options
+  var oTable = $('#data-table-container').DataTable({
+    'order' : [[0, 'asc'], [3, 'desc'], [1, 'asc']],
+    'columns': createTableColumns(),
+     'ajax': {
+     url: 'output.json',
+     cache: true,
+       'dataSrc': function(json) {
+         var myType;
+         var allAuthors;
+         var pubDate;
+         var myData = json['results']; //spreadsheet data lives in an array with the name values
+         //rewrite data to an object with key-value pairs. This is also a chance to rename or ignore columns
+         myData= myData.map(function( n, i ) {
+          //handle type - third entry is usually better than the second but not always present
+           if (n['document_type'][2]) {
+             myType = n['document_type'][2]
+            } else {
+              myType = n['document_type'][1]
+          };
+          //list all authors
+          allAuthors = n['author'].join(", ");
+          pubDate = new Date(n['publication_date']);
+          console.log(pubDate);
+          pubDate = pubDate.getFullYear();
+          console.log(pubDate);
+
+
+             var myObject = {
+               url:n['url'],
+               title:n['title'],
+               type:myType,
+               author:allAuthors,
+               year:pubDate
+             };
+             return myObject;
+         });
+         myData.splice(0,1); //remove the first row, which contains the orginal column headers
+         return myData;
+       }
+     },
+     rowGroup: {
+       dataSrc: 'type'
+   }
+
+  });
+
+});
